7e88b2817e7bc2f9cdfb1ecd456923ae51111b85
Valdiney V GOMES <valdiney.gomes@dafiti.com.br>
2021-04-28 18:14:08 -0300

    A ordem dos checkups deve ser preservada no log

--- src/main/java/br/com/dafiti/hanger/service/JobCheckupLogService.java
+++ src/main/java/br/com/dafiti/hanger/service/JobCheckupLogService.java
@@ -30,7 +30,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import br.com.dafiti.hanger.repository.JobCheckupLogRepository;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
